4d2a608089f017f3fcafa482ce41c25834b360c2
Endel Dreyer <endel.dreyer@gmail.com>
2019-01-01 13:17:43 -0200

    improve lobby/castle tiles

--- client/behaviors/BattleBehaviour.js
+++ client/behaviors/BattleBehaviour.js
@@ -37,10 +37,6 @@ export default class BattleBehaviour extends Behaviour {
   onAttack (data) {
     if (!data.type) { return this.disable(); }
 
-    // // TODO: this shouldn't be necessary
-    // // GameObject's patch method is triggering 'attack' event multiple times
-    // if (this.togglePositionTimeout.active) { return; }
-
     this.togglePosition = true
     this.togglePositionTimeout = App.clock.setTimeout(() => { this.togglePosition = false }, 100)
 
