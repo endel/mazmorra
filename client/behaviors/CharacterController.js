908428e119c01e553e179bf0c074414a7f7bedce
Endel Dreyer <endel.dreyer@gmail.com>
2019-06-09 13:54:53 -0300

    improving configs

--- client/behaviors/CharacterController.js
+++ client/behaviors/CharacterController.js
@@ -34,25 +34,28 @@ export default class CharacterController extends Behaviour {
     // this.camera.position.y = lerp(this.camera.position.y, this.originalY + 30, 0.1) // this.camera.position.y = lerp(this.camera.position.y, this.originalY + 15, 0.1)
     // this.camera.position.z = lerp(this.camera.position.z, this.object.position.z + 50, 0.1)
 
-    // 3rd person (Orthographic)
-    this.camera.position.x = lerp(this.camera.position.x, this.object.position.x + 90 * Math.cos( 1 ), 0.1) // + (window.innerWidth / window.innerHeight)
-    this.camera.position.z = lerp(this.camera.position.z, this.object.position.z + 50, 0.1)
-    this.camera.position.y = lerp(this.camera.position.y, this.originalY + 40, 0.1)
+    // 3rd person (Orthographic) (OLD ORTOGRAPHIC, W/ HIDDEN OBJECTS BEHIND WALLS)
+    // this.camera.position.x = lerp(this.camera.position.x, this.object.position.x + 90 * Math.cos( 1 ), 0.1) // + (window.innerWidth / window.innerHeight)
+    // this.camera.position.z = lerp(this.camera.position.z, this.object.position.z + 50, 0.1)
+    // this.camera.position.y = lerp(this.camera.position.y, this.originalY + 40, 0.1)
+
+    this.camera.position.x = lerp(this.camera.position.x, this.object.position.x + 90 * Math.cos( 1 ), 0.05) // + (window.innerWidth / window.innerHeight)
+    this.camera.position.z = lerp(this.camera.position.z, this.object.position.z + 40, 0.1)
+    this.camera.position.y = lerp(this.camera.position.y, this.originalY + 50, 0.05)
 
     // // Perspective
     // this.camera.position.x = lerp(this.camera.position.x, this.object.position.x + 20, 0.1)
     // this.camera.position.z = lerp(this.camera.position.z, this.object.position.z + 20, 0.1)
     // this.camera.position.y = lerp(this.camera.position.y, this.originalY + 20, 0.1)
 
-
     //  + 30 * Math.cos( 1 )
     this.lookAtTarget.x = lerp(this.lookAtTarget.x, this.object.position.x, 0.1)
     this.lookAtTarget.y = lerp(this.lookAtTarget.y, this.object.position.y, 0.1)
     this.lookAtTarget.z = lerp(this.lookAtTarget.z, this.object.position.z, 0.1)
 
-    // this.lookAtTarget.x = lerp(this.lookAtTarget.x, this.object.position.x, 0.1)
-    // this.lookAtTarget.y = lerp(this.lookAtTarget.y, this.object.position.y, 0.1)
-    // this.lookAtTarget.z = lerp(this.lookAtTarget.z, this.object.position.z, 0.1)
+    // this.lookAtTarget.x = this.object.position.x;
+    // this.lookAtTarget.y = this.object.position.y;
+    // this.lookAtTarget.z = this.object.position.z;
 
     this.camera.lookAt(this.lookAtTarget)
   }
