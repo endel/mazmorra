8db889bc679434d3899626759a00def691d58f9d
Endel Dreyer <endel.dreyer@gmail.com>
2019-07-13 16:34:44 -0300

    hint inventoy hotkeys

--- client/elements/inventory/OpenButton.js
+++ client/elements/inventory/OpenButton.js
@@ -53,7 +53,7 @@ export default class OpenInventoryButton extends THREE.Object3D {
   }
 
   onMouseOver () {
-    Hint.show("Inventory", this);
+    Hint.show("(I / B) Inventory", this);
 
     App.tweens.remove(this.scale)
     App.tweens.add(this.scale).to({ x: 1.1, y: 1.1 }, 200, Tweener.ease.quadOut)
