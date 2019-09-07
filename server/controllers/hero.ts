50b15b4839e12c68c8f6391d5ed2b026bc029722
Endel Dreyer <endel.dreyer@gmail.com>
2019-09-07 16:20:36 -0300

    upgrade colyseus to 0.11.10

--- server/controllers/hero.ts
+++ server/controllers/hero.ts
@@ -3,24 +3,23 @@ import helpers from "../../shared/helpers"
 import { jwtMiddleware } from "@colyseus/social/express";
 
 import { Hero, ATTRIBUTE_BASE_VALUE, DBItem } from "../db/Hero";
-import { Attribute } from "../entities/Unit";
 
 export const router = express.Router()
 
 router.get('/', jwtMiddleware, async (req: express.Request, res: express.Response) => {
-  const heroes = await Hero.find({ userId: req.auth._id, alive: true });
+  const heroes = await Hero.find({ userId: req.cauth._id, alive: true });
   res.json(heroes);
 });
 
 router.delete('/', jwtMiddleware, async (req, res) => {
   // delete previous user's heroes.
   const deleted = await Hero.deleteOne({
-    userId: req.auth._id,
+    userId: req.cauth._id,
     _id: req.body._id
   });
 
   if (deleted.n > 0) {
-    const heroes = await Hero.find({ userId: req.auth._id, alive: true });
+    const heroes = await Hero.find({ userId: req.cauth._id, alive: true });
     res.json(heroes);
 
   } else {
@@ -69,12 +68,12 @@ router.post('/', jwtMiddleware, async (req, res) => {
   const primaryAttribute = primaryAttributes[req.body.klass];
 
   // delete previous user's heroes.
-  const numHeroes = await Hero.count({ userId: req.auth._id });
+  const numHeroes = await Hero.count({ userId: req.cauth._id });
 
   // each player can only have 3 heroes
   if (numHeroes < 3) {
     res.json(await Hero.create({
-      userId: req.auth._id,
+      userId: req.cauth._id,
 
       name: req.body.name.substr(0, 20),
       klass: req.body.klass,
